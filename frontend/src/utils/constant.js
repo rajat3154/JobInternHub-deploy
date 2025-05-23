4fadfa3168a6b8923ca61f42105eeaefe0850c1c
rajat3154 <rajaturanvir31@gmail.com>
2025-05-23 20:48:55 +0530

    .

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -3,6 +3,7 @@ const BACKEND_URL = 'https://jobinternhub.onrender.com';
 
 // Configure axios defaults
 import axios from 'axios';
+axios.defaults.baseURL = BACKEND_URL;
 axios.defaults.withCredentials = true;
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 
@@ -87,11 +88,11 @@ export const getAuthToken = () => {
   return localStorage.getItem('token');
 };
 
-export const USER_API_END_POINT = `${BACKEND_URL}/api/v1`;
-export const STUDENT_API_END_POINT = `${BACKEND_URL}/api/v1/student`;
-export const RECRUITER_API_END_POINT = `${BACKEND_URL}/api/v1/recruiter`;
-export const JOB_API_END_POINT = `${BACKEND_URL}/api/v1/job`;
-export const INTERNSHIP_API_END_POINT = `${BACKEND_URL}/api/v1/internship`;
-export const APPLICATION_API_END_POINT = `${BACKEND_URL}/api/v1/application`;
-export const CHAT_API_END_POINT = `${BACKEND_URL}/api/v1/chat`;
+export const USER_API_END_POINT = '/api/v1';
+export const STUDENT_API_END_POINT = '/api/v1/student';
+export const RECRUITER_API_END_POINT = '/api/v1/recruiter';
+export const JOB_API_END_POINT = '/api/v1/job';
+export const INTERNSHIP_API_END_POINT = '/api/v1/internship';
+export const APPLICATION_API_END_POINT = '/api/v1/application';
+export const CHAT_API_END_POINT = '/api/v1/chat';
 export const SOCKET_URL = BACKEND_URL;
