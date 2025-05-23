0392640747348331397ce02dcefd4d2c24dd6ab7
rajat3154 <rajaturanvir31@gmail.com>
2025-05-23 19:47:45 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -19,8 +19,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-      origin: [`${process.env.FRONTEND_URL}`],
-      credentials: true
+      origin: [
+            process.env.FRONTEND_URL,
+            'https://jobinternhub.vercel.app',
+            'http://localhost:5173'
+      ],
+      credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization']
 };
 app.use(cors(corsOptions));
 
